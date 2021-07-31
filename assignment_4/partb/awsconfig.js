3c964480755f7b85aead1de7971d79b68a44041f
Dhruv Doshi <dh722257@dal.ca>
2021-07-30 23:55:34 -0300

    Update awsconfig.js

--- assignment_4/partb/awsconfig.js
+++ assignment_4/partb/awsconfig.js
@@ -5,10 +5,10 @@ let s3;
 
 const setS3Obj = () => new BlueBirdPromise((res) => {
     AWS.config.update({
-        region: "us-east-1",
-        String ACCESS_KEY="ASIA3TAPEDRZAPNXAP7N";
-        String SECRET_KEY="yjR14wRL8/Z79ak8MPm+EirgyXYZJhSZsIrZ9HYI";
-        String ACCESS_TOKEN="FwoGZXIvYXdzELv//////////wEaDGnBYbIhkPx8fo63HiK/AUwCBM1jSY7H31GWy0TVm4u9w35BUHsXHh0ouViM4WL3CBMIWMYa3tkXMmJjHYBij9v2AD1WPzf47Fq8Qyv/6eIN6BppzCxigva2j5erKZ5DrPcvEhLb1NfciwqKSbfLv0yhY6HNqfcJatB2Th9EDgAEPGHOZHhrfbXJsT8WaVtZx0w5I9BIAZxA36Gtktu9gGfQ8M8i7xqLcoxQTli5Hk0gGA8YLOkXi4IIlYjDCw/KpEmHjpWDkaOyuTAx+3BwKPnQkogGMi2+XSFnSGKbWzPsgP+cw6v3Ts2TM3pNvBKE8LnSB8zLv31/N2YXrmJKCRgCt9Y=";
+    accessKeyId: "ASIA3TAPEDRZAPNXAP7N",
+    secretAccessKey: "yjR14wRL8/Z79ak8MPm+EirgyXYZJhSZsIrZ9HYI",
+    sessionToken: "FwoGZXIvYXdzELv//////////wEaDOUIRai5ESbyL0orKyK/Aa8Xb3caHzEAnn4DgQotKAM3dPLm/s2Bq7CJX/EL2Uc6G9fQ7OG57R88OVT2zaV7idQTjh7HvLUR1dh+kkhSdt3X8zZgcpCnkccwwIf7vX7bXxhjhAG65BpSU6HnQIjE40wAXMPqdMccNEe3wgtQoYN+x6PziUsipYo6lrRRgWK+fWW3eYP3AiaxlFmyTlhGY0h9CV02kBGT56htV0jJ4dn+a6VB7ntBfNoyYRDYEgnxIe1ayhCMK+sFC7/jxRSPKIfVkogGMi1PSzxTCGQPlfaIyf9yajh6f5qb3gmmL3ke14z5Cl6jYoxxiaSxQBdEhhYJqLw=",
+    region: "us-east-1"
 
     })
 
